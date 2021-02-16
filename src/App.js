c992c8f165e15ef457c3d7873d1d96eb210e3153
Jacob Whisler <jwhisler521@gmail.com>
2021-02-16 16:12:22 -0500

    imported Header and GlobalStyle components and implemented them into the App function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Header from "./Components/Header";
+import GlobalStyle from "./Components/GlobalStyle";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
 };
 
 export default App;
